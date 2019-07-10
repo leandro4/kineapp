c873b161ee0d7f583c19439ba70e21dbb5a85ac2
Franco Consoni <franco@goprebo.com>
2019-07-10 09:50:33 -0300

    CHANGE: refactored profile view

--- app/src/main/java/com/gon/kineapp/ui/fragments/ProfileFragment.kt
+++ app/src/main/java/com/gon/kineapp/ui/fragments/ProfileFragment.kt
@@ -1,12 +1,14 @@
 package com.gon.kineapp.ui.fragments
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import com.bumptech.glide.Glide
 import com.gon.kineapp.R
 import com.gon.kineapp.mvp.presenters.LoginPresenter
 import com.gon.kineapp.mvp.views.LoginView
@@ -18,13 +20,12 @@ import com.google.android.gms.common.ConnectionResult
 import com.google.android.gms.common.GoogleApiAvailability
 import com.google.android.gms.common.api.ApiException
 import kotlinx.android.synthetic.main.fragment_login.*
+import kotlinx.android.synthetic.main.fragment_login.etPhone
+import kotlinx.android.synthetic.main.fragment_profile.*
 
 class ProfileFragment: BaseMvpFragment(), LoginView {
 
     var presenter = LoginPresenter()
-
-    private var googleSignInClient: GoogleSignInClient? = null
-    private val RC_SIGN_IN = 1000
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return LayoutInflater.from(context).inflate(R.layout.fragment_profile, container, false)
@@ -37,94 +38,11 @@ class ProfileFragment: BaseMvpFragment(), LoginView {
     }
 
     private fun initUI() {
-        fabLogin.setOnClickListener {
-            if (checkPlayServices() /*&& validateFields()*/) {
-                val signInIntent = googleSignInClient?.signInIntent
-                activity?.startActivityForResult(signInIntent, RC_SIGN_IN)
-            }
-        }
 
-        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken("1093191472549-9gk2os2g3hm2qa1bhrhr1ab0cl7r5qkb.apps.googleusercontent.com")
-            .requestEmail()
-            .build()
 
-        googleSignInClient = GoogleSignIn.getClient(activity!!, gso)
-    }
+        Glide.with(context!!).load(Uri.parse("https://www.elpopular.pe/sites/default/files/styles/img_620x465/public/imagen/2018/10/21/Noticia-218698-hombre-se-llevo-sorpresa-al-descubrir-que-su-perrito-era-un-animal-de-otra-especie.jpg?itok=3a9Eb3oL"))
+            .into(civAvatar)
 
-    private fun validateFields(): Boolean {
-        var mandatoryField = true
-        if (etName.text.toString().isEmpty()) {
-            etName.error = "Campo Obligatorio"
-            mandatoryField = false
-        }
-        if (etLastName.text.toString().isEmpty()) {
-            etLastName.error = "Campo Obligatorio"
-            mandatoryField = false
-        }
-        if (etIdNumber.text.toString().isEmpty()) {
-            etIdNumber.error = "Campo Obligatorio"
-            mandatoryField = false
-        }
-        if (etAddress.text.toString().isEmpty()) {
-            etAddress.error = "Campo Obligatorio"
-            mandatoryField = false
-        }
-        if (etPhone.text.toString().isEmpty()) {
-            etPhone.error = "Campo Obligatorio"
-            mandatoryField = false
-        }
-        return mandatoryField
-    }
-
-    private fun checkPlayServices(): Boolean {
-        val googleAPI = GoogleApiAvailability.getInstance()
-        val result = googleAPI.isGooglePlayServicesAvailable(context)
-
-        if (result != ConnectionResult.SUCCESS) {
-            val PLAY_SERVICES_RESOLUTION_REQUEST = 1000
-            //Google Play Services app is not available or version is not up to date. Error the
-            if (googleAPI.isUserResolvableError(result)) {
-                googleAPI.getErrorDialog(
-                    activity, result,
-                    PLAY_SERVICES_RESOLUTION_REQUEST
-                ).show()
-            }
-
-            if (result == ConnectionResult.SERVICE_UPDATING)
-                Toast.makeText(context, getString(R.string.generic_wait_message), Toast.LENGTH_SHORT).show()
-
-            return false
-        }
-        return true
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        if (resultCode == AppCompatActivity.RESULT_OK && requestCode == RC_SIGN_IN) {
-
-            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
-            try {
-                val account = task.getResult(ApiException::class.java)
-
-                Toast.makeText(context, account?.displayName, Toast.LENGTH_SHORT).show()
-
-                //presenter.requestLogin()
-                onLoginSuccess()
-
-            } catch (e: ApiException) {
-                Toast.makeText(context, getString(R.string.generic_error_message), Toast.LENGTH_SHORT).show()
-            }
-
-        } else if (resultCode == AppCompatActivity.RESULT_CANCELED) {
-            Toast.makeText(context, getString(R.string.generic_error_message), Toast.LENGTH_SHORT).show()
-        }
-    }
-
-    private fun goToHome() {
-        activity?.startActivity(Intent(activity, PatientListActivity::class.java))
-        activity?.finish()
     }
 
     override fun startPresenter() {
@@ -141,7 +59,6 @@ class ProfileFragment: BaseMvpFragment(), LoginView {
     }
 
     override fun onLoginSuccess() {
-        goToHome()
     }
 
     override fun onLoginFailure() {
